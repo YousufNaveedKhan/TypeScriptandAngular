76e9ed7ab8d7d1b424d32b0fffe6345b50c0915e
YousufNaveedKhan <yk033127@gmail.com>
2026-06-02 20:17:40 +0500

    New: Lec01 of TypeScript and Angular Uploaded

---
+++ TypeScript/lec01/app.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "Laiba";
+console.log(userName);
+let age = 20;
+console.log(age);
+let stdNames = ["Abdul Rehman", "Abu Hurerah", "Anum Akram", "Syeda Anusha Madani", "Laiba Ajaz"];
+console.log(stdNames[1]);
+let empNames = ["Hammad", 20];
+console.log(empNames);
---
+++ TypeScript/lec01/app.ts
@@ -0,0 +1,11 @@
+let userName:string = "Laiba";
+console.log(userName);
+
+let age:number = 20;
+console.log(age);
+
+let stdNames:string[] = ["Abdul Rehman", "Abu Hurerah", "Anum Akram", "Syeda Anusha Madani", "Laiba Ajaz"]; 
+console.log(stdNames[1]);
+
+let empNames:[string, number] = ["Hammad", 20];
+console.log(empNames);
